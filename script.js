3effa91c7c60f420aa134762d32450c3abb206d9
jonahhowell <jonahowell800@gmail.com>
2022-12-09 14:06:56 -0500

    Create grid kinda works

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function createGrid(length) {
-    
+    const container = document.getElementById('maincontainer');
+    for (let i = 0; i < length; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.style.cssText = 'flex: 1;'
+        for (let j = 0; j < length; j++) {
+            const colDiv = document.createElement('div');
+            colDiv.textContent = j;
+            rowDiv.appendChild(colDiv);
+        }
+        container.appendChild(rowDiv);
+    }
 }
+
+createGrid(16);
