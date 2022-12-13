d4bdb9133ac699fcca02bf374fd17cda79f163e4
jonahhowell <jonahowell800@gmail.com>
2022-12-13 12:44:24 -0500

    Created function to append mouseover event listener

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+const ROWDIVSTYLE = 'display: flex; flex: 1; justify-content: space-between;';
+const COLDIVSTYLE = 'flex: 1;'
+const COLDIVHOVEREDSTYLE = 'flex: 1; background-color: black;'
+
+
 function createGrid(length) {
     const container = document.getElementById('maincontainer');
     for (let i = 0; i < length; i++) {
         const rowDiv = document.createElement('div');
-        rowDiv.style.cssText = 'display: flex; flex: 1; justify-content: space-between;'
+        rowDiv.style.cssText = ROWDIVSTYLE;
         for (let j = 0; j < length; j++) {
             const colDiv = document.createElement('div');
-            colDiv.style.cssText = 'flex: 1;'
-            colDiv.textContent = j;
+            colDiv.style.cssText = COLDIVSTYLE;
+            setToBlackOnHover(colDiv);
             rowDiv.appendChild(colDiv);
         }
         container.appendChild(rowDiv);
     }
 }
 
-createGrid(16);
+function setToBlackOnHover(colDiv) {
+    colDiv.addEventListener("mouseover", () => {
+        colDiv.style.cssText = COLDIVHOVEREDSTYLE;
+    })
+}
+
+createGrid(9);
