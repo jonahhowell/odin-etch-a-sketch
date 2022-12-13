8b6489a3ec9b277f5f7997784ff8600d6313b142
jonahhowell <jonahowell800@gmail.com>
2022-12-13 12:04:15 -0500

    createGrid creates an even square board

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function createGrid(length) {
     const container = document.getElementById('maincontainer');
     for (let i = 0; i < length; i++) {
         const rowDiv = document.createElement('div');
-        rowDiv.style.cssText = 'flex: 1;'
+        rowDiv.style.cssText = 'display: flex; flex: 1; justify-content: space-between;'
         for (let j = 0; j < length; j++) {
             const colDiv = document.createElement('div');
+            colDiv.style.cssText = 'flex: 1;'
             colDiv.textContent = j;
             rowDiv.appendChild(colDiv);
         }
